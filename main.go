97a244359cf9f0916d26eb6d89b228d3f913cea9
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-03-26 22:20:40 +0530

    Initial commit

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func main() {
+	clientset := getKubernetesClientSet()
+
+	nsList, _ := clientset.CoreV1().
+		Namespaces().
+		List(context.Background(), metav1.ListOptions{})
+
+	for _, n := range nsList.Items {
+		fmt.Println(n.Name)
+	}
 }
