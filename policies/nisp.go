8cc3d1c7fdd8934106b103a59252394a95aef647
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-15 18:05:31 +0530

    Implemented DeactivateNode function and refactor comments in nisp

--- policies/nisp.go
+++ policies/nisp.go
@@ -9,9 +9,10 @@ import (
 // NISP implements the IPolicyNodes interface
 type NISP struct{}
 
+// ExecuteForCluster
+// Assumption: optional containers are deployed in nodes that are labelled as optional
+// These nodes do not contain mandatory containers
 func (nisp NISP) ExecuteForCluster() {
-	//# Assumption: optional containers are deployed in nodes that are labelled as optional
-	//# These nodes do not contain mandatory containers
 	sortedNodes := kubernetesCluster.GetNodesSortedCPUUsage(constants.OPTIONAL)
 	allNodes := sortedNodes
 	nisp.executePolicy(allNodes, sortedNodes)
@@ -24,9 +25,7 @@ func (nisp NISP) executePolicy(allNodes []string, sortedNodes []string) {
 
 	for predictedPower > constants.UPPER_THRESHOLD_POWER {
 		i++
-
-		// get the nodes remaining in the cluster after deactivating i nodes
-		predictedClusterNodes := SliceDifference(allNodes, sortedNodes[0:i])
+		predictedClusterNodes := SliceDifference(allNodes, sortedNodes[0:i]) // get the nodes remaining in the cluster after deactivating i nodes
 
 		// TODO integrate with the powerModel package
 		fmt.Println(predictedClusterNodes)
@@ -34,29 +33,22 @@ func (nisp NISP) executePolicy(allNodes []string, sortedNodes []string) {
 	}
 
 	if (constants.UPPER_THRESHOLD_POWER-predictedPower)/constants.UPPER_THRESHOLD_POWER < 0.1 {
-		// deactivate all pods of 0 to i hosts
-		nisp.deactivateNodes(sortedNodes[0:i])
+		nisp.deactivateNodes(sortedNodes[0:i]) // deactivate all pods of 0 to i hosts
 		return
 	}
 
-	// Can set policy (LUCF, LRU, RCSP)
-	var policy IPolicyPods = LUCF{}
+	var policy IPolicyPods = LUCF{} // Can set policy (LUCF, LRU, RCSP)
 
 	if i == 1 {
-		// deactivate some containers of 0th node according to one of the pod selection policies
-		policy.ExecuteForNode(sortedNodes[0])
+		policy.ExecuteForNode(sortedNodes[0]) // deactivate some containers of 0th node according to a pod selection policy
 	} else {
-		// deactivate all containers of 0 to i-1 hosts
-		nisp.deactivateNodes(sortedNodes[0 : i-1])
-
-		// deactivate some containers of ith node according to one of the pod selection policies
-		policy.ExecuteForNode(sortedNodes[i])
+		nisp.deactivateNodes(sortedNodes[0 : i-1]) // deactivate all containers of 0 to i-1 hosts
+		policy.ExecuteForNode(sortedNodes[i])      // deactivate some containers of ith node according to a pod selection policy
 	}
 }
 
 func (nisp NISP) deactivateNodes(nodeList []string) {
-	// TODO implement this function in kubernetesCluster package
-	//for node := range nodeList {
-	//	kubernetesCluster.DeactivateNode(node)
-	//}
+	for _, node := range nodeList {
+		kubernetesCluster.DeactivateNode(node, constants.NAMESPACE, constants.OPTIONAL)
+	}
 }
