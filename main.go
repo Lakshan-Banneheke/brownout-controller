1985c65fddfaa21ecbf1cfdd20674b725d43bfac
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-19 16:24:35 +0530

    Add GetErrorRequestCount

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import "brownout-controller/prometheus"
 func main() {
 
 	prometheus.GetTotalRequestCount("podinfo.localdev.me", "1d")
-
+	prometheus.GetErrorRequestCount("podinfo.localdev.me", "1d")
 }
--- prometheus/sli.go
+++ prometheus/sli.go
@@ -13,3 +13,12 @@ func GetTotalRequestCount(hostname string, interval string) {
 	reqCount := int(vectorVal.Value)
 	log.Printf("Total Request Count for host %s in the last %s: %v", hostname, interval, reqCount)
 }
+
+// GetErrorRequestCount interval parameter can have 1m, 30m, 1d, etc
+func GetErrorRequestCount(hostname string, interval string) {
+	query := "sum by (host) (increase(nginx_ingress_controller_requests{status=~'[4-5].*', host=~'" + hostname + "'}[" + interval + "]))"
+	result := doQuery(query)              // Result is of type Vector
+	vectorVal := result.(model.Vector)[0] // Cast to mode.Vector and get the first row
+	reqCount := int(vectorVal.Value)
+	log.Printf("Total Error Request Count for host %s in the last %s: %v", hostname, interval, reqCount)
+}
