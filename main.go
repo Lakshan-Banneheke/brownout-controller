2018a7e4fa7d6247fcdc7304e7515711128bcf52
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-19 16:33:37 +0530

    Add GetErrorRequestCount

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import "brownout-controller/prometheus"
 
 func main() {
 
+	prometheus.GetTotalRequestCount("podinfo.localdev.me", "1d")
+
 	prometheus.GetSlowRequestCount("podinfo.localdev.me", "1d")
 
 }
