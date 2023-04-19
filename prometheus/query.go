795caf3e109ac40df3f5b306b296db798cb0fa0a
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-19 16:09:23 +0530

    Update GetTotalRequestCount

--- prometheus/query.go
+++ prometheus/query.go
@@ -2,7 +2,6 @@ package prometheus
 
 import (
 	"context"
-	"fmt"
 	"github.com/prometheus/common/model"
 	"log"
 	"time"
@@ -10,11 +9,11 @@ import (
 	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
 )
 
-func doQuery(q string) model.Value {
+func doQuery(query string) model.Value {
 
 	v1api := getV1API()
 
-	result, warnings, err := v1api.Query(context.Background(), q, time.Now(), v1.WithTimeout(30*time.Second))
+	result, warnings, err := v1api.Query(context.Background(), query, time.Now(), v1.WithTimeout(30*time.Second))
 
 	if err != nil {
 		log.Printf("Error querying Prometheus: %v\n", err)
@@ -22,10 +21,6 @@ func doQuery(q string) model.Value {
 	if len(warnings) > 0 {
 		log.Printf("Warnings: %v\n", warnings)
 	}
-	vectorVal := result.(model.Vector)
-	for _, elem := range vectorVal {
-		fmt.Println(elem.Metric)
-		fmt.Println(elem.Value)
-	}
+
 	return result
 }
--- prometheus/sli.go
+++ prometheus/sli.go
@@ -1,10 +1,15 @@
 package prometheus
 
-import "log"
+import (
+	"github.com/prometheus/common/model"
+	"log"
+)
 
 // GetTotalRequestCount interval parameter can have 1m, 30m, 1d, etc
 func GetTotalRequestCount(hostname string, interval string) {
 	query := "sum by (host) (increase(nginx_ingress_controller_requests{host=~\"" + hostname + "\"}[" + interval + "]))"
-	result := doQuery(query)
-	log.Printf("Query Result:\n%v\n", result)
+	result := doQuery(query)              // Result is of type Vector
+	vectorVal := result.(model.Vector)[0] // Cast to mode.Vector and get the first row
+	reqCount := int(vectorVal.Value)
+	log.Printf("Request Count: %v", reqCount)
 }
