c5a7e865490d3669c6908b61f02cda9335cfeff8
dhaura <dhaurapathirana@gmail.com>
2023-04-19 21:57:52 +0530

    temp

--- main.go
+++ main.go
@@ -1,26 +1,25 @@
 package main
 
 import (
-	"brownout-controller/brownout"
 	"brownout-controller/constants"
-	"brownout-controller/powerModel"
-	"brownout-controller/prometheus"
+	"brownout-controller/kubernetesCluster"
 	"fmt"
-	"log"
 )
 
 func main() {
 
-	// get the power model
-	pm := powerModel.GetPowerModel()
-
-	// get power consumption when a set of pods given
-	log.Println(pm.GetPowerConsumptionPods([]string{"agri-app-master-75656cf88b-fcd29", "agri-app-master-75656cf88b-xtkl4", "agri-app-master-75656cf88b-hxplj"}))
-	// get power consumption when a set of nodes given
-	log.Println(pm.GetPowerConsumptionNodes([]string{"node-master", "node-worker-1"}))
-
-	fmt.Println(prometheus.GetSLAViolationRatio("podinfo.localdev.me", "1d", constants.SLA_VIOLATION_LATENCY))
-	fmt.Println(prometheus.GetSLASuccessRatio("podinfo.localdev.me", "1d", constants.SLA_VIOLATION_LATENCY))
-
-	brownout.ExecuteBrownout()
+	//// get the power model
+	//pm := powerModel.GetPowerModel()
+	//
+	//// get power consumption when a set of pods given
+	//log.Println(pm.GetPowerConsumptionPods([]string{"agri-app-master-75656cf88b-fcd29", "agri-app-master-75656cf88b-xtkl4", "agri-app-master-75656cf88b-hxplj"}))
+	//// get power consumption when a set of nodes given
+	//log.Println(pm.GetPowerConsumptionNodes([]string{"node-master", "node-worker-1"}))
+	//
+	//fmt.Println(prometheus.GetSLAViolationRatio("podinfo.localdev.me", "1d", constants.SLA_VIOLATION_LATENCY))
+	//fmt.Println(prometheus.GetSLASuccessRatio("podinfo.localdev.me", "1d", constants.SLA_VIOLATION_LATENCY))
+	//
+	//brownout.ExecuteBrownout()
+	fmt.Println(kubernetesCluster.GetPodNamesAll(constants.NAMESPACE))
+	fmt.Println(kubernetesCluster.GetPodNamesCategory(constants.NAMESPACE, constants.OPTIONAL))
 }
