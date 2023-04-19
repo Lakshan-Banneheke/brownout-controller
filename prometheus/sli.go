711d08a4ba0258058503d353c55132d8b39440db
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-19 15:35:47 +0530

    Update GetTotalRequestCount

--- prometheus/sli.go
+++ prometheus/sli.go
@@ -4,7 +4,7 @@ import "log"
 
 // GetTotalRequestCount interval parameter can have 1m, 30m, 1d, etc
 func GetTotalRequestCount(hostname string, interval string) {
-	query := "sum by (host) (increase(nginx_ingress_controller_requests{host=~\"" + hostname + "\"}[" + interval + "]))"
+	query := "sum(increase(nginx_ingress_controller_requests{host=~\"" + hostname + "\"}[" + interval + "]))"
 	result := doQuery(query)
 	log.Printf("Query Result:\n%v\n", result)
 }
