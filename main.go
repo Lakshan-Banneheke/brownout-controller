cfa88acecaace5ee36bd407612730996525059a9
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-19 15:34:42 +0530

    Add GetTotalRequestCount

--- main.go
+++ main.go
@@ -4,55 +4,6 @@ import "brownout-controller/prometheus"
 
 func main() {
 
-	//Nodes need to be given the label category=optional via kubectl label nodes <your-node-name> category=optional
-	//nodeNames := kubernetesCluster.GetNodeNames("optional")
-	//nodesCPU := kubernetesCluster.GetNodesSortedCPUUsage("optional")
-	//
-	//log.Println("==============Nodes ===================")
-	//log.Println(nodeNames)
-	//log.Println(nodesCPU)
-	//
-	//podNames := kubernetesCluster.GetPodNames("default", "optional")
-	//podsCPUSorted := kubernetesCluster.GetPodsSortedCPUUsageAll("default", "optional")
-	//
-	//log.Println("==============All Pods ===================")
-	//log.Println(podNames)
-	//log.Println(podsCPUSorted)
-	//
-	//log.Println("==============Instance 4===================")
-	//podsCPUSortedInstance4 := kubernetesCluster.GetPodsSortedCPUUsageInNode("instance-4", "default", "optional")
-	//log.Println(podsCPUSortedInstance4)
-	//log.Println("==============Instance 5===================")
-	//podsCPUSortedInstance5 := kubernetesCluster.GetPodsSortedCPUUsageInNode("instance-5", "default", "optional")
-	//log.Println(podsCPUSortedInstance5)
-	//
-	//deactivatedDeploymentMap := kubernetesCluster.DeactivatePods([]string{
-	//	"nginx-cd55c47f5-jlhq8", "nginx-cd55c47f5-kkqtc",
-	//	"traefik-7c57d8789b-dn29b", "traefik-7c57d8789b-pp824"}, "default")
-	//
-	//log.Println(deactivatedDeploymentMap)
-	//
-	//deployments := map[string]int32{"nginx": 10, "traefik": 5}
-	//kubernetesCluster.ActivatePods(deployments, "default")
-
-	//policies.LUCF{}.ExecuteForCluster()
-
-	//lucf := policies.LUCF{}
-	//lucf.ExecuteForCluster()
-
-	//var t policies.IPolicy
-	//t = policies.LUCF{}
-	//t.ExecuteForCluster()
-
-	// get the power model
-	//pm := powerModel.GetPowerModel("v4")
-	//
-	//// get power consumption when a set of pods given
-	//log.Println(pm.GetPowerConsumptionPods([]string{"agri-app-master-75656cf88b-kmxvs", "agri-app-master-75656cf88b-rn72n", "agri-app-master-75656cf88b-wtp82"}))
-	// get power consumption when a set of nodes given
-	//log.Println(pm.GetPowerConsumptionNodes([]string{"test-kubernetes-controller-1"}))
-
-	q := "sum by (exported_namespace, exported_service, host, le) (increase(nginx_ingress_controller_request_duration_seconds_bucket{host=~\"podinfo.localdev.me\"}[1d]))"
-	prometheus.DoQuery(q)
+	prometheus.GetTotalRequestCount("podinfo.localdev.me", "1d")
 
 }
---
+++ prometheus/sli.go
@@ -0,0 +1,10 @@
+package prometheus
+
+import "log"
+
+// GetTotalRequestCount interval parameter can have 1m, 30m, 1d, etc
+func GetTotalRequestCount(hostname string, interval string) {
+	query := "sum by (host) (increase(nginx_ingress_controller_requests{host=~\"" + hostname + "\"}[" + interval + "]))"
+	result := doQuery(query)
+	log.Printf("Query Result:\n%v\n", result)
+}
