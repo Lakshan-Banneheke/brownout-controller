6ba37d9b90e66566d8744b7e4c5e2bba5a7963c0
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-19 15:45:06 +0530

    Update GetTotalRequestCount

--- prometheus/query.go
+++ prometheus/query.go
@@ -2,6 +2,7 @@ package prometheus
 
 import (
 	"context"
+	"fmt"
 	"github.com/prometheus/common/model"
 	"log"
 	"time"
@@ -10,14 +11,17 @@ import (
 )
 
 func doQuery(q string) model.Value {
+
 	v1api := getV1API()
 
 	result, warnings, err := v1api.Query(context.Background(), q, time.Now(), v1.WithTimeout(30*time.Second))
+
 	if err != nil {
 		log.Printf("Error querying Prometheus: %v\n", err)
 	}
 	if len(warnings) > 0 {
 		log.Printf("Warnings: %v\n", warnings)
 	}
+	fmt.Println(result.Type())
 	return result
 }
