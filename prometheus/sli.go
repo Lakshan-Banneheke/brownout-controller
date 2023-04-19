ee2fc1ce76f1d080ae1ba61808cf5270d34afa70
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-19 16:32:27 +0530

    Add GetErrorRequestCount

--- prometheus/sli.go
+++ prometheus/sli.go
@@ -1,6 +1,7 @@
 package prometheus
 
 import (
+	"fmt"
 	"github.com/prometheus/common/model"
 	"log"
 )
@@ -12,4 +13,14 @@ func GetTotalRequestCount(hostname string, interval string) {
 	vectorVal := result.(model.Vector)[0] // Cast to mode.Vector and get the first row
 	reqCount := int(vectorVal.Value)
 	log.Printf("Total Request Count for %s in the last %s: %v", hostname, interval, reqCount)
+}
+
+// GetSlowRequestCount interval parameter can have 1m, 30m, 1d, etc
+func GetSlowRequestCount(hostname string, interval string) {
+	query := "sum by (le) (increase(nginx_ingress_controller_request_duration_seconds_bucket{host=~\"" + hostname + "\"}[" + interval + "]))"
+	result := doQuery(query) // Result is of type Vector
+	fmt.Println(result)
+	//vectorVal := result.(model.Vector)[0] // Cast to mode.Vector and get the first row
+	//reqCount := int(vectorVal.Value)
+	//log.Printf("Total Request Count for %s in the last %s: %v", hostname, interval, reqCount)
 }
