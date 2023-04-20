210394bedaea7848282894143b6c8d102452b8fc
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-20 14:14:08 +0530

    temp

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"brownout-controller/constants"
 	"brownout-controller/policies/experimentation"
+	"os"
+	"strconv"
 )
 
 func main() {
 
 	//fmt.Println(prometheus.GetSLAViolationRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
 	//fmt.Println(prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
-	experimentation.LUCFExperiment(constants.REQUIRED_SR)
+	requiredSR := os.Getenv("REQUIRED_SR")
+	requiredSRFloat, _ := strconv.ParseFloat(requiredSR, 32)
+	experimentation.LUCFExperiment(requiredSRFloat)
 
 }
