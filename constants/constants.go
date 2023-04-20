dbd0e107c41d2594271ce691e7e54f8f8b306b8a
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-20 16:45:14 +0530

    Set latency 0.1

--- constants/constants.go
+++ constants/constants.go
@@ -1,9 +1,9 @@
 package constants
 
 const (
-	OPTIONAL  = "optional"
-	NAMESPACE = "default"
-	//UPPER_THRESHOLD_POWER         = 5000
+	OPTIONAL                      = "optional"
+	NAMESPACE                     = "default"
+	UPPER_THRESHOLD_POWER         = 5000
 	SLA_VIOLATION_LATENCY         = "0.1" //seconds
 	SLA_INTERVAL                  = "1m"
 	ACCEPTED_SUCCESS_RATE         = 0.75
