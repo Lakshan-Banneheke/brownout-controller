473507bf71175c86661d6edcfb4a0cfa7eb995f7
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-21 03:42:29 +0530

    Refactor GetPodsInNode

--- main.go
+++ main.go
@@ -3,26 +3,30 @@ package main
 import (
 	"brownout-controller/constants"
 	"brownout-controller/kubernetesCluster"
+	"brownout-controller/policies"
+	"brownout-controller/policies/experimentationv2"
 	"brownout-controller/powerModel"
-	"log"
+	"brownout-controller/prometheus"
+	"fmt"
 )
 
 func main() {
 
-	allNodes := kubernetesCluster.GetAllNodeNames()
-	log.Println(allNodes)
-	log.Println(len(allNodes))
-	log.Println(powerModel.GetPowerModel().GetPowerConsumptionNodes(allNodes))
-	pods := kubernetesCluster.GetPodNamesAll(constants.NAMESPACE)
-	log.Println("Initial Power: ", powerModel.GetPowerModel().GetPowerConsumptionPods(pods))
+	//allNodes := kubernetesCluster.GetAllNodeNames()
+	//log.Println(allNodes)
+	//log.Println(len(allNodes))
+	//log.Println(powerModel.GetPowerModel().GetPowerConsumptionNodes(allNodes))
+	//pods := kubernetesCluster.GetPodNamesAll(constants.NAMESPACE)
+	//log.Println("Initial Power: ", powerModel.GetPowerModel().GetPowerConsumptionPods(pods))
+
 	//thresholds := []float64{19, 18.5, 18, 17.5, 17, 16.5, 16}
 
-	//pods := kubernetesCluster.GetPodNamesAll(constants.NAMESPACE)
-	//fmt.Println("Initial Power: ", powerModel.GetPowerModel().GetPowerConsumptionPods(pods))
-	//prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY)
-	//
-	//nisp := policies.NISP{}
-	//experimentationv2.DoExperiment(nisp, 19)
+	pods := kubernetesCluster.GetPodNamesAll(constants.NAMESPACE)
+	fmt.Println("Initial Power: ", powerModel.GetPowerModel().GetPowerConsumptionPods(pods))
+	prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY)
+
+	nisp := policies.NISP{}
+	experimentationv2.DoExperiment(nisp, 19)
 
 	//fmt.Println(prometheus.GetSLAViolationRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
 	//fmt.Println(prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
