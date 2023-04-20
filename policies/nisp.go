8b6e9b09a70cc12dd450f3c9c2e1ec7ac48220b6
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-21 03:19:41 +0530

    Experiment

--- policies/nisp.go
+++ policies/nisp.go
@@ -5,6 +5,7 @@ import (
 	"brownout-controller/kubernetesCluster"
 	"brownout-controller/powerModel"
 	"brownout-controller/util"
+	"log"
 )
 
 // NISP implements the IPolicyNodes interface
@@ -27,14 +28,19 @@ func (nisp NISP) executePolicy(allNodes []string, sortedNodes []string, upperThr
 	var predictedPower float64 = 0
 
 	for predictedPower > upperThresholdPower {
+		log.Println("===============================================================")
+		log.Println("Iteration: ", i)
 		i++
 		predictedClusterNodes := util.SliceDifference(allNodes, sortedNodes[0:i]) // get the nodes remaining in the cluster after deactivating i nodes
 
 		// get power consumption of the nodes
 		predictedPower = powerModel.GetPowerModel().GetPowerConsumptionNodes(predictedClusterNodes)
+		log.Println("Predicted Power", predictedPower)
+		log.Println("Upper Threshold", upperThresholdPower)
 	}
 
 	if (upperThresholdPower-predictedPower)/upperThresholdPower < 0.05 {
+		log.Printf("Exact node count used. Deactivating a;; pods in %v nodes", i)
 		nisp.deactivateNodes(sortedNodes[0:i]) // deactivate all pods of 0 to i hosts
 		return nodeDeployments
 	}
@@ -42,9 +48,12 @@ func (nisp NISP) executePolicy(allNodes []string, sortedNodes []string, upperThr
 	var policy IPolicyPods = LUCF{} // Can set policy (LUCF, LRU, RCSP)
 
 	if i == 1 {
+		log.Printf("i = 1. Executing LUCF for 1st node")
 		return policy.ExecuteForNode(sortedNodes[0], upperThresholdPower) // deactivate some containers of 0th node according to a pod selection policy
 	} else {
-		nisp.deactivateNodes(sortedNodes[0 : i-1])                                       //  node_deployments                             // deactivate all containers of 0 to i-1 hosts
+		log.Printf("i = %v. Deactivating all pods in %v nodes", i, i-1)
+		nisp.deactivateNodes(sortedNodes[0 : i-1])
+		log.Printf("Executing LUCF for %vth node", i)                                    //  node_deployments                             // deactivate all containers of 0 to i-1 hosts
 		oneNodeDeployments := policy.ExecuteForNode(sortedNodes[i], upperThresholdPower) // deactivate some containers of ith node according to a pod selection policy
 		addToNodeDeployments(oneNodeDeployments)
 
