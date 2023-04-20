e6e452cd56cb3b62ad6a1f097269f8343d781369
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-20 16:33:39 +0530

    Experiment

--- prometheus/sla.go
+++ prometheus/sla.go
@@ -10,12 +10,12 @@ func GetSLAViolationRatio(hostname string, interval string, latency string) floa
 	reqSlow := getSlowRequestCount(hostname, interval, latency)
 
 	slaViolationRatio := float64(reqSlow+reqError) / float64(reqTotal)
-	log.Printf("SLA violation ratio for host %s", slaViolationRatio)
+	log.Printf("SLA violation ratio for host %v", slaViolationRatio)
 	return slaViolationRatio
 }
 
 func GetSLASuccessRatio(hostname string, interval string, latency string) float64 {
 	slaSuccessRatio := 1 - GetSLAViolationRatio(hostname, interval, latency)
-	log.Printf("SLA Success ratio for host %s", slaSuccessRatio)
+	log.Printf("SLA Success ratio for host %v", slaSuccessRatio)
 	return slaSuccessRatio
 }
