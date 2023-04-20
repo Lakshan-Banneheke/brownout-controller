65d53b34a2c88025da59343bbd9b562981eef3b9
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-20 16:25:40 +0530

    Experiment

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"brownout-controller/constants"
 	"brownout-controller/kubernetesCluster"
 	"brownout-controller/powerModel"
+	"brownout-controller/prometheus"
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -12,5 +14,10 @@ func main() {
 	//fmt.Println(prometheus.GetSLAViolationRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
 	//fmt.Println(prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
 	pods := kubernetesCluster.GetPodNamesAll(constants.NAMESPACE)
-	fmt.Println(powerModel.GetPowerModel().GetPowerConsumptionPods(pods))
+	for {
+		fmt.Println(powerModel.GetPowerModel().GetPowerConsumptionPods(pods))
+		fmt.Println(prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
+		time.Sleep(30 * time.Second)
+	}
+
 }
