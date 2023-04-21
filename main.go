0cfa0d258720371d7b62e9cbba66a7e0f553ce04
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-21 20:30:18 +0530

    Experiment

--- main.go
+++ main.go
@@ -18,10 +18,10 @@ func main() {
 	fmt.Println("Initial Power: ", powerModel.GetPowerModel().GetPowerConsumptionPods(pods))
 	prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY)
 
-	nisp := policies.NISP{}
-	//hucf := policies.HUCF{}
-	//experimentationv2.DoExperimentPodPolicies(hucf, 16)
-	experimentationv2.DoExperimentNodePolicies(nisp, 12)
+	//nisp := policies.NISP{}
+	hucf := policies.HUCF{}
+	experimentationv2.DoExperimentPodPolicies(hucf, 16)
+	//experimentationv2.DoExperimentNodePolicies(nisp, 12)
 
 	//fmt.Println(prometheus.GetSLAViolationRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
 	//fmt.Println(prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
