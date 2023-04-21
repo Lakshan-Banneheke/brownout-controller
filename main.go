5a050565a196ce1303ed1b99a190f5f2b9431cb6
dhaura <dhaurapathirana@gmail.com>
2023-04-21 13:59:42 +0530

    Experiment

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY)
 
 	policy := policies.RCSP{}
-	experimentationv2.DoExperiment(policy, 19)
+	experimentationv2.DoExperiment(policy, 18.75)
 
 	//fmt.Println(prometheus.GetSLAViolationRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
 	//fmt.Println(prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
