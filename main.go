61666be5e9f0da2f8736a5315b645948bb99cdd8
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-21 19:12:05 +0530

    Experiment

--- main.go
+++ main.go
@@ -12,13 +12,6 @@ import (
 
 func main() {
 
-	//allNodes := kubernetesCluster.GetAllNodeNames()
-	//log.Println(allNodes)
-	//log.Println(len(allNodes))
-	//log.Println(powerModel.GetPowerModel().GetPowerConsumptionNodes(allNodes))
-	//pods := kubernetesCluster.GetPodNamesAll(constants.NAMESPACE)
-	//log.Println("Initial Power: ", powerModel.GetPowerModel().GetPowerConsumptionPods(pods))
-
 	//thresholds := []float64{19, 18.5, 18, 17.5, 17, 16.5, 16}
 
 	pods := kubernetesCluster.GetPodNamesAll(constants.NAMESPACE)
@@ -28,7 +21,7 @@ func main() {
 	nisp := policies.NISP{}
 	//hucf := policies.HUCF{}
 	//experimentationv2.DoExperimentPodPolicies(hucf, 16)
-	experimentationv2.DoExperimentNodePolicies(nisp, 14)
+	experimentationv2.DoExperimentNodePolicies(nisp, 13)
 
 	//fmt.Println(prometheus.GetSLAViolationRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
 	//fmt.Println(prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
