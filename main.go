c8f2a6f1c949089a3956564300c124ba3b18bb62
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-22 18:43:59 +0530

    Experiment

--- main.go
+++ main.go
@@ -12,11 +12,11 @@ func main() {
 	//fmt.Println("Initial Power: ", powerModel.GetPowerModel().GetPowerConsumptionPods(pods))
 	//prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY)
 
-	//nisp := policies.NISP{}
-	rcsp := policies.RCSP{}
+	nisp := policies.NISP{}
+	//rcsp := policies.RCSP{}
 
-	experimentationv2.DoBrownoutExperimentPodPolicy(rcsp, constants.K_RSCP)
-	//experimentationv2.DoBrownoutExperimentNodePolicy(nisp, constants.K_NISP)
+	//experimentationv2.DoBrownoutExperimentPodPolicy(rcsp, constants.K_RSCP)
+	experimentationv2.DoBrownoutExperimentNodePolicy(nisp, constants.K_NISP)
 
 	//experimentationv2.DoExperimentPodPolicies(policy, 12)
 
