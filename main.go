5a4eee96dd53f7c0b2fb160d0a2fc3c798c177ba
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-04-22 14:09:04 +0530

    Experiment

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ func main() {
 	//thresholds := []float64{19, 18.5, 18, 17.5, 17, 16.5, 16}
 
 	//nisp := policies.NISP{}
-	policy := policies.RCSP{}
-	experimentationv2.DoExperimentPodPolicies(policy, 12)
+	policy := policies.LUCF{}
+	experimentationv2.DoExperimentPodPolicies(policy, 14)
 	//experimentationv2.DoExperimentNodePolicies(nisp, 12)
 
 	//fmt.Println(prometheus.GetSLAViolationRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
