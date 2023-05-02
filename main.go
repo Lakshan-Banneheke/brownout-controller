132a111c1a1d3a64d3934df5f4d8eb7cf6a8b085
Lakshan Banneheke <62496951+Lakshan-Banneheke@users.noreply.github.com>
2023-05-02 16:03:10 +0530

    Cleanup main.go

--- main.go
+++ main.go
@@ -1,32 +1,7 @@
 package main
 
-import (
-	"brownout-controller/experimentation"
-	"os"
-	"strconv"
-)
+import "brownout-controller/api"
 
 func main() {
-
-	policy := os.Getenv("policy") // get policy name from env variables eg: NISP
-	uts := os.Getenv("UT")        // get upper threshold from env variables
-	ut, _ := strconv.ParseFloat(uts, 64)
-	experimentation.DoExperimentNodePolicies(policy, ut)
-
-	//pods := kubernetesCluster.GetPodNamesAll(constants.NAMESPACE)
-	//fmt.Println("Initial Power: ", powerModel.GetPowerModel().GetPowerConsumptionPods(pods))
-	//prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY)
-
-	////experimentation.DoBrownoutExperimentPodPolicy("RCSP" constants.K_RCSP)
-	//experimentation.DoBrownoutExperimentNodePolicy("NISP", constants.K_NISP)
-
-	//fmt.Println(prometheus.GetSLAViolationRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
-	//fmt.Println(prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY))
-	//pods := kubernetesCluster.GetPodNamesAll(constants.NAMESPACE)
-	//for {
-	//	fmt.Println("Current Power: ", powerModel.GetPowerModel().GetPowerConsumptionPods(pods))
-	//	prometheus.GetSLASuccessRatio(constants.HOSTNAME, constants.SLA_INTERVAL, constants.SLA_VIOLATION_LATENCY)
-	//	time.Sleep(30 * time.Second)
-	//}
-
+	api.InitAPI()
 }
